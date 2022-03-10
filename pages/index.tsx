4e0cc8a9a2547c920a586bcdfc53296aeb1f81c8
Justice Velasco <justicevgomez001@gmail.com>
2022-03-11 00:51:43 +0100

    🔨 UPDATED: Global CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,36 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import FooterComponent from '../components/FooterComponent'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import FooterComponent from "../components/FooterComponent";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Justice Velasco</title>
-        <meta name="description" content="Justice Velasco (Web and mobile developer)" />
+        <meta
+          name="description"
+          content="Justice Velasco (Web and mobile developer)"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="/projects">Next.js!</a>
-        </h1>
+        <article className={styles.contentMain}>
+          <h1 className={styles.Maintitle}>Justice Velasco</h1>
+          <p className={styles.mainParagraph}>
+            The little front end web/mobile developer.
+          </p>
+        </article>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <article className={styles.aboutMe} id="about-me">
+          <h1>Acerca de mi</h1>
+        </article>
       </main>
 
-      <FooterComponent/>
-      
+      <FooterComponent />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
