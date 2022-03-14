e499101f1ed22d89fe3fcf94ee599ed4b6e021b9
Justice Velasco <justicevgomez001@gmail.com>
2022-03-14 20:09:04 +0100

    ✨ CREATED: Created by

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,13 @@ const Home: NextPage = () => {
         </article>
         <AboutMe/>
         <Projects/>
+        <div className={styles.logoContainer}>
+          <h3 style={{textAlign:"center", color:"#2c2c2c"}}>Web creada con:</h3>
+          <div className={styles.logoContent}>
+            <img src="https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png" alt="logo react" className={styles.logo} />
+            <img src="https://miro.medium.com/max/1400/1*Smbj_VLH7JRp9GhLaKyiUQ.png" alt="logo react" className={styles.logo} />
+          </div>
+        </div>
       </main>
 
       <FooterComponent />
