c7372d265a57cbf088922a3ee62e45cf8cadcf31
Justice Velasco <justicevgomez001@gmail.com>
2022-05-08 19:55:42 +0200

    🔨 UPDATED: Styles updated

--- components/Presentation/index.tsx
+++ components/Presentation/index.tsx
@@ -7,9 +7,7 @@ function Presentation() {
   return (
     <div className={styles.presentationContainer}>
       <div className={styles.content}>
-        <h1 className="font-manrope mt-[4px] text-[40px] text-transparent font-bold bg-gradient-to-r from-purple-600 via-cyan-400 to-pink-500 bg-clip-text w-max leading-[48px] md:text-[64px] md:leading-[86px] xl:text-[90px] lg:mx-0 lg:text-left lg:leading-[110px]">
-          Justice Velasco
-        </h1>
+        <h1 className={styles.mainTitle}>Justice Velasco</h1>
         <h2>Desarrollador web/mobile.</h2>
         <p>
           Soy una persona incontenible por aprender algo nuevo cada vez que
@@ -18,10 +16,8 @@ function Presentation() {
         </p>
       </div>
 
-      <div className={styles.image}>
-        <div
-          className="p-[2px] bg-gradient-to-tr from-violet-700 to-rose-500"
-        >
+      <div className={styles.imageContainer}>
+        <div className={styles.image}>
           <Image
             src="https://jusapp.s3.eu-central-1.amazonaws.com/linkedin.jpg"
             alt="justice velasco"
